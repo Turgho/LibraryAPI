c7b35e91f45a1065333d98a70122c095c5619a33
Turgho <victor.hugo3692111@gmail.com>
2025-08-08 15:28:23 -0300

    Feat: Add Required fields to Title and Author

--- DTOs/UpdateBookDto.cs
+++ DTOs/UpdateBookDto.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BibliotecaAPI.DTOs;
 
 public class UpdateBookDto
 {
+    [Required]
     public string? Title { get; set; }
+    [Required]
     public string? Author { get; set; }
     public int? PublicationYear { get; set; }
-    public bool? AvailableForLoan { get; set; }
+    public bool AvailableForLoan { get; set; }
 }
