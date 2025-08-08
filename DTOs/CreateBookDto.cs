eb066629cd1f022634ca14f12af3b1e1f72f7e9c
Turgho <victor.hugo3692111@gmail.com>
2025-08-08 15:24:45 -0300

    Fix: Changed CreateBookDto Record to Class, with required fields

--- DTOs/CreateBookDto.cs
+++ DTOs/CreateBookDto.cs
@@ -1,10 +1,17 @@
-using BibliotecaAPI.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace BibliotecaAPI.DTOs;
 
-public record CreateBookDto(
-    string Title,
-    string Author,
-    int PublicationYear,
-    bool AvailableForLoan
-);
+public class CreateBookDto
+{
+    [Required(ErrorMessage = "O título é obrigatório.")]
+    public string Title { get; set; } = String.Empty;
+
+    [Required(ErrorMessage = "O autor é obrigatório.")]
+    public string Author { get; set; } = String.Empty;
+
+    [Range(1000, 2100, ErrorMessage = "Ano inválido.")]
+    public int PublicationYear { get; set; }
+
+    public bool AvailableForLoan { get; set; }
+}
